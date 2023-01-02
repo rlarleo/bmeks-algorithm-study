294992e8839972c245723285d3e7e08ab6930aa7
rlarleo <soulen2403@naver.com>
2023-01-03 01:14:49 +0900

    ACM 호텔: 수학, 구현

---
+++ Class2/10250.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void TieCin()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);
+}
+
+void Output(int x, int y)
+{
+    if(y < 10)
+        cout << x << '0' << y << endl;
+    else
+        cout << x << y << endl;
+}
+
+int main()
+{
+    TieCin();
+
+    int test, height, width, host;
+    int x, y;
+    
+    cin >> test;
+    
+    for(int i = 0; i < test; i++)
+    {
+        cin >> height >> width >> host;
+
+        x = host % height ? host % height : height;
+        y = (host-1) / height + 1;
+
+        Output(x, y);
+    }
+
+    return 0;
+}
